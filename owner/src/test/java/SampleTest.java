5fc21f23071facfe3fa651478985653fd49a1876
mrfox <mrfox@devadmin.com>
2020-04-13 21:48:31 +0300

    Added Owner functional Added classes related to influxDB functional

--- owner/src/test/java/SampleTest.java
+++ owner/src/test/java/SampleTest.java
@@ -1,19 +1,20 @@
+import config.ServerConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 
-@Listeners(ExecutionListner.class)
 public class SampleTest {
 
     protected static WebDriver driver;
     private Logger logger = LogManager.getLogger(SampleTest.class);
+    private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
 
     @BeforeTest
     public void setUp() {
@@ -23,7 +24,7 @@ public class SampleTest {
     }
     @Test
     public void openPage() {
-        driver.get("https://otus.ru/");
+        driver.get(cfg.url());
         logger.info("Открыта страница отус");
     }
 
